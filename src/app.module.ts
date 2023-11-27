2f5a9f2bf8bd780f8914e9c5cfe84ee294120668
Bhat Owais <owaisbhat996@outlook.com>
2023-11-27 14:47:43 +0530

    Updated config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
+import { mongooseCreds } from "./config/mongoose";
 
 @Module({
   imports: [
     ChatsModule,
     MongooseModule.forRoot(
-      'mongodb+srv://ovaixe:passMe404@cluster0.pppcttj.mongodb.net/?retryWrites=true&w=majority',
+      mongooseCreds.URL,
     ),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
