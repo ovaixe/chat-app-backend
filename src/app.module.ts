e28ecfa3387e73ca6446a0509cef21ba54af9e9d
Bhat Owais <owaisbhat996@outlook.com>
2024-02-16 01:24:21 +0530

    Added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatsModule } from './chats/chats.module';
 import { AuthModule } from './auth/auth.module';
-import { mongooseCreds } from './config/mongoose';
 
 @Module({
-  imports: [ChatsModule, MongooseModule.forRoot(mongooseCreds.URL), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+    ChatsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
